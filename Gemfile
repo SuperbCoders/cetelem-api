4a95ca5b786f7eb2c027e0b0dd4b1e08e0e76b23
Alexxfrolov <alex99-999@yandex.ru>
2019-11-28 10:17:53 +0300

    implement .env

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'bcrypt-ruby', '~> 3.1.2'
 gem 'jwt_sessions'
 gem 'rails', '~> 6.0.1'
 # Use postgresql as the database for Active Record
+gem 'dotenv-rails'
 gem 'pg', '>= 0.18', '< 2.0'
 gem 'redis-rails'
 # Use Puma as the app server
--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,10 @@ require 'active_storage/engine'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Dotenv::Railtie.load
+
+HOSTNAME = ENV['HOSTNAME']
+
 module Cetelem
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
