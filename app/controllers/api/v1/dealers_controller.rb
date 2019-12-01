2efe113ae216a9a17bd5d45b46b8db4812582492
Alexey Frolov <alexeyfrolov@Alexeys-MacBook-Pro.local>
2019-12-01 14:28:09 +0300

    fix delaer cars

--- app/controllers/api/v1/dealers_controller.rb
+++ app/controllers/api/v1/dealers_controller.rb
@@ -3,20 +3,20 @@
 class Api::V1::DealersController < ApplicationController
   def show
     find_dealer do |dealer|
-      render json: dealer.as_json
+      render json: dealer
     end
   end
 
   def reservations
     return head 400 unless Dealer.exists?(id: params[:dealer_id])
 
-    render json: Reservation.for_dealer(params[:dealer_id]).as_json
+    render json: Reservation.for_dealer(params[:dealer_id])
   end
 
   def cars
     return head 400 unless Dealer.exists?(id: params[:id])
 
-    render json: DealerCar.available.where(dealer_id: params[:id]).as_json
+    render json: DealerCar.available.where(dealer_id: params[:id])
   end
 
   def upload_xml
