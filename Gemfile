e0416c76741095d3029f213bad0261ac61a64f58
Alexey Frolov <alexeyfrolov@Alexeys-MacBook-Pro.local>
2019-12-01 10:18:24 +0300

    add sidekiq for background

--- Gemfile
+++ Gemfile
@@ -31,7 +31,10 @@ gem 'activerecord-import'
 gem 'bootsnap', '>= 1.4.2', require: false
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
-# gem 'rack-cors'
+gem 'rack-cors'
+
+# for backgroud jobs
+gem 'sidekiq'
 
 group :development, :test do
   gem 'factory_bot_rails'
--- spec/jobs/dealer_car_upload_job_spec.rb
+++ spec/jobs/dealer_car_upload_job_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe DealerCarUploadJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let (:dealer) { create(:dealer) }
+  before do
+    CarImport.call("#{Rails.root}/spec/fixtures/cars.xml")
+    YAML::load(File.open("#{Rails.root}/db/data/extra_options.yaml")).each do |type, names|
+      ExtraOption.import(names.map { |name| { name: name, type: type } })
+    end
+  end
+
+  it "uploads data" do
+    dealer.xmls.attach(
+      io: File.open("#{fixture_path}/data.xml"),
+      filename: 'data.xml',
+      content_type: 'application/xml'
+    )
+
+    described_class.perform_now(dealer.xmls.last, dealer)
+    expect(dealer.xmls.last.status).to eq("done")
+    expect(dealer.dealer_cars.count).to eq(2)
+  end
 end
