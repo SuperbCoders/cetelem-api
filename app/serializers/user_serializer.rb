2b624bb4d3dc44bbc2d753f5535ed7be285af1bc
Alexxfrolov <alex99-999@yandex.ru>
2019-12-17 12:06:09 +0300

    time stamps to user

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :login, :role, :dealer_id
+  attributes :id, :login, :role, :dealer_id, :updated_at, :created_at
 end
