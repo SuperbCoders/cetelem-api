1fe40d93ca7ae1ae7989ff9fb0b86cabbe9485d3
Alexey Frolov <alexeyfrolov@Alexeys-MacBook-Pro.local>
2019-12-19 00:25:07 +0300

    fix current loc

--- app/controllers/api/v1/current_location_controller.rb
+++ app/controllers/api/v1/current_location_controller.rb
@@ -3,8 +3,16 @@
 class Api::V1::CurrentLocationController < ApplicationController
 
   def index
-    data = Geocoder.search(request.ip, params: { lang: :ru }).first&.data
+    data = Geocoder.search(request.ip, params: { lang: :ru }).first&.data || {}
 
-    render json: data.as_json(only: %w[country regionName city zip lat lon])
+    data_json =
+      {
+        country: data['country'],
+        region: data['regionName'],
+        city: data['city'],
+        postcode: data['postcode']
+      }
+
+    render json: data_json
   end
 end
