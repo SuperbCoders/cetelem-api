0b7745cc126d5ce5140ffe11169aa34e6b6aa89d
Alexxfrolov <alex99-999@yandex.ru>
2019-12-19 17:19:11 +0300

    fix admin/statistics

--- app/controllers/api/v1/admin/statistics_controller.rb
+++ app/controllers/api/v1/admin/statistics_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Api::V1::Admin::ReservationsController < ApplicationController
+class Api::V1::Admin::StatisticsController < ApplicationController
   before_action do
     authorize_by_access_cookie!
     authenticate_for_role(:admin)
